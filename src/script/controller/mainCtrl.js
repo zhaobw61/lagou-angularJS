010742908b3036d62e3d5a9cba5a27e1176c105e
zhaobw61 <1964471757@qq.com>
2019-10-29 14:30:33 +0800

    完成职位列表得数据绑定

--- src/script/controller/mainCtrl.js
+++ src/script/controller/mainCtrl.js
@@ -1,2 +1,21 @@
-angular.module('app').controller('mainCtrl',['$scope',function(){
+'use strict';
+angular.module('app').controller('mainCtrl',['$scope',function($scope){
+    $scope.list = [{
+        id: '1',
+        name: '销售',
+        imgSrc: 'image/company-3.png',
+        companyName: '千度',
+        cityName: '上海',
+        industryName: '互联网',
+        time: '2016-06-01 11:05'
+    },{
+        id: '2',
+        name: 'WEB销售',
+        imgSrc: 'image/company-1.png',
+        companyName: '慕课网',
+        cityName: '北京',
+        industryName: '互联网',
+        time: '2016-06-01 12:05'
+    }]
+    $scope.name = 'zhaobowen'
 }])
