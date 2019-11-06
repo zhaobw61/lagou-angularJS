0af062aebc105ce02077b2b16ac05a09f40217cc
zhaobw61 <1964471757@qq.com>
2019-11-06 20:25:52 +0800

    初始化公司详情

--- src/script/config/router.js
+++ src/script/config/router.js
@@ -7,6 +7,10 @@ angular.module('app').config(['$stateProvider','$urlRouterProvider',function($st
       url: '/position/:id',
       templateUrl: 'view/position.html',
       controller: 'positionCtrl'
+    }).state('company',{
+      url:'/company/:id',
+      templateUrl:'view/company.html',
+      controller: 'companyCtrl'
     });
     $urlRouterProvider.otherwise('main');
 }])
---
+++ src/script/controller/companyCtrl.js
@@ -0,0 +1,3 @@
+'use strict';
+angular.module('app').controller('companyCtrl',['$scope',function($scope){
+}])
