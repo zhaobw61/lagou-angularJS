1b29a24b1cef621478765311d4b7740fefad6f0b
zhaobw61 <1964471757@qq.com>
2019-11-07 15:28:41 +0800

    添加首页数据

--- src/script/controller/mainCtrl.js
+++ src/script/controller/mainCtrl.js
@@ -1,21 +1,7 @@
 'use strict';
-angular.module('app').controller('mainCtrl',['$scope',function($http,$scope){
-    $scope.list = [{
-        id: '1',
-        name: '销售',
-        imgSrc: 'image/company-3.png',
-        companyName: '千度',
-        cityName: '上海',
-        industryName: '互联网',
-        time: '2016-06-01 11:05'
-    },{
-        id: '2',
-        name: 'WEB销售',
-        imgSrc: 'image/company-1.png',
-        companyName: '慕课网',
-        cityName: '北京',
-        industryName: '互联网',
-        time: '2016-06-01 12:05'
-    }]
+angular.module('app').controller('mainCtrl',['$http','$scope',function($http,$scope){
+    $http.get('/data/positionList.json').success(function(resp){
+        $scope.list = resp;
+    })
     $scope.name = 'zhaobowen'
 }])
