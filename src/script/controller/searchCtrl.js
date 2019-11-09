603cc85cce927e4e0383bdf938ef6aa8b123f924
zhaobw <1964471757@qq.com>
2019-11-09 19:12:55 +0800

    搜索头部布局

--- src/script/controller/searchCtrl.js
+++ src/script/controller/searchCtrl.js
@@ -1,4 +1,6 @@
 'use strict';
 angular.module('app').controller('searchCtrl',['$http','$state','$scope',function($http,$state,$scope){
-    
+  $http.get('data/positionList.json').success(function(resp){
+    $scope.positionList = resp;
+  })
 }])
---
+++ src/script/directive/tab.js
@@ -0,0 +1,7 @@
+angular.module('app').directive('appTab',[function(){
+  return {
+      restrict: 'A',
+      replace: true,
+      templateUrl: 'view/template/tab.html'
+  }
+}]);
