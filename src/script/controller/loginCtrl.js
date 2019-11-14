3a4f6e9d906b8df0b9124c84520578753a6a11ea
zhaobw61 <1964471757@qq.com>
2019-11-14 16:06:33 +0800

    完成用户登录的模块

--- src/script/controller/loginCtrl.js
+++ src/script/controller/loginCtrl.js
@@ -1,4 +1,11 @@
 'use strict';
-angular.module('app').controller('loginCtrl',['$http','$scope',function($http,$scope){
-    
+angular.module('app').controller('loginCtrl',['cache','$state','$http','$scope',function(cache,$state, $http,$scope){
+    $scope.submit = function(){
+        $http.post('data/login.json',$scope.user).success(function(resp){
+            cache.put('id',rep.id);
+            cache.put('name',rep.name);
+            cache.put('image',rep.image);
+            $state.go('main');
+        })
+    }
 }])
--- src/script/directive/head.js
+++ src/script/directive/head.js
@@ -1,7 +1,6 @@
 'use strict'
 angular.module('app').directive('appHead',['cache', function(cache){
     cache.testConsole('heade')
-    console.log(cache)
     return {
         restrict: 'A',
         replace: true,
